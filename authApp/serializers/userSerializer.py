8c3279e8d637a3674a0d622b6e1e23bcfaad4633
Ronrook <78703403+Ronrook@users.noreply.github.com>
2021-11-17 21:31:16 -0500

    Crear serializador

--- authApp/serializers/userSerializer.py
+++ authApp/serializers/userSerializer.py
@@ -1,7 +1,8 @@
-from rest_framework import serializers
-from authApp.models.user import User
 
-class UserSerializer(serializers.ModelSerializer):
+from rest_framework       import serializers # Importacion de la libreria serializadora
+from authApp.models.user  import User        #  Importacion de la clase User 
+
+class UserSerializer(serializers.ModelSerializer): # Creamos la clase serializadora
     class Meta:
-        model = User
-        fields = '__all__'
+        model = User  # Usamos el modelo User 
+        fields = '__all__' # Se serializan todos los campos de la clase 
