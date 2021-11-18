8d547fd506a220d933f089dcee7837cb938b8fed
Edwincala <86900578+Edwincala@users.noreply.github.com>
2021-11-17 23:03:28 -0500

    Configuración sitio de administración

--- authApp/admin.py
+++ authApp/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from .models.user import User
 
 # Register your models here.
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'nombres', 'apellidos', 'email', 'tipo_documento', 'documento', 'telefono', 'lic_cond', 'is_staff')
+    list_filter = ('documento', 'lic_cond', 'nombres')
+    search_fields = ('documento', 'username', 'nombres', 'lic_cond', 'apellidos')
+
+
+#admin.site.register(User)
